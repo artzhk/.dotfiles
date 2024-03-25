f14d6b922ebc64906fcb2d263f1c5c3d9b6c1a77
Artem3124 <mr.zhukovsky02@gmail.com>
2024-03-25 23:27:38 +0100

    wezterm migrated

---
+++ .wezterm.lua
@@ -0,0 +1,18 @@
+local wezterm = require 'wezterm'
+
+local config = wezterm.config_builder()
+
+config.color_scheme = 'Rosé Pine (Gogh)'
+
+config.force_reverse_video_cursor = true
+config.hide_tab_bar_if_only_one_tab = true
+
+config.font = wezterm.font_with_fallback {
+  {
+    family = 'Ubuntu Mono',
+    weight = 'Regular',
+    harfbuzz_features = { 'calt=0', 'clig=0', 'liga=0' },
+  },
+}
+
+return config
