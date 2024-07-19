9c32c37b7d7d290be7c2e9779307d55b48252834
Artem3124 <mr.zhukovsky02@gmail.com>
2024-07-19 15:16:21 +0200

    fonts added

--- upload_files.sh
+++ upload_files.sh
@@ -34,6 +34,7 @@ if [[ $os == "macos" ]]; then
 fi 
 
 if [[ $os == "archlinux" ]] ; then
+    cp -v /usr/local/share/fonts/ttf/* ~/.dotfiles/fonts/
     cp -v ~/.bashrc ~/.dotfiles/linux/
     cp -v ~/.bash_profile ~/.dotfiles/linux/
     cp -v ~/.zshrc ~/.dotfiles/linux/
