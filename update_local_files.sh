43628f0cba496b4e8b3259e8bca4892f32467f6c
Artem3124 <mr.zhukovsky02@gmail.com>
2024-09-16 17:05:18 +0200

    feat(updating_system): updating system process improved

--- update_local_files.sh
+++ update_local_files.sh
@@ -11,9 +11,9 @@ echo "Select the OS you are using: "
 
 select os in "archlinux" "macos"; do
     case $os in
-        archlinux ) os="archlinux"; break;;
-        macos ) os="macos"; break;;
-        android ) os="android"; break;;
+        archlinux ) os="archlinux"; break ;;
+        macos ) os="macos"; break ;;
+        android ) os="android"; break ;;
     esac
 done
 
@@ -21,37 +21,39 @@ echo "You selected $os"
 
 echo "${GREEN}Updating dotfiles for $os... ${NC}"
 
-cp -v ~/.dotfiles/.tmux.conf ~/
-cp -v ~/.dotfiles/.vimrc ~/
-if [[ $os != "android" ]]; then
-    cp -vr ~/.dotfiles/alacritty ~/.config/
-fi
-mkdir -p ~/.oh-my-zsh/themes && 
-    cp -vr ~/.dotfiles/.oh-my-zsh/themes ~/.oh-my-zsh/
-mkdir -p ~/.local/scripts/ && 
-    cp -v ~/.dotfiles/scripts/* ~/.local/scripts/
+get_diff_and_copy ~/.dotfiles/.tmux.conf ~/
+get_diff_and_copy ~/.dotfiles/.vimrc ~/
 
-if [[ $os == "macos" ]]; then 
-    cp -v ~/.dotfiles/macos/.bashrc ~/
-    cp -v ~/.dotfiles/macos/.bash_profile ~/
-    cp -v ~/.dotfiles/macos/.zshrc ~/
-    cp -vr ~/.dotfiles/macos/aerospace ~/.config/
-fi 
+if [[ $os != "android" ]]; then
+    get_diff_and_copy ~/.dotfiles/alacritty ~/.config/
+fi
+
+mkdir -p ~/.oh-my-zsh/themes
+get_diff_and_copy ~/.dotfiles/.oh-my-zsh/themes ~/.oh-my-zsh/
+mkdir -p ~/.local/scripts/
+get_diff_and_copy ~/.dotfiles/scripts/* ~/.local/scripts/
+
+if [[ $os == "macos" ]]; then
+    get_diff_and_copy ~/.dotfiles/macos/.bashrc ~/
+    get_diff_and_copy ~/.dotfiles/macos/.bash_profile ~/
+    get_diff_and_copy ~/.dotfiles/macos/.zshrc ~/
+    get_diff_and_copy ~/.dotfiles/macos/aerospace ~/.config/
+fi
 
 if [[ $os == "archlinux" ]] ; then
-    cp -v ~/.dotfiles/linux/clipton ~/.config/
-    cp -v ~/.dotfiles/linux/.bashrc ~/
-    cp -v ~/.dotfiles/linux/.bash_profile ~/
-    cp -v ~/.dotfiles/linux/.zshrc ~/
-    cp -vr ~/.dotfiles/linux/polybar ~/.config/
-    cp -v ~/.dotfiles/linux/.xinitrc ~/
-    cp -v ~/.dotfiles/linux/.Xresources ~/
-    cp -vr ~/.dotfiles/linux/bat ~/.config/
-    cp -vr ~/.dotfiles/linux/kitty ~/.config/
-    cp -vr ~/.dotfiles/linux/rofi ~/.config/
-    cp -vr ~/.dotfiles/linux/spicetify/Themes/text/color.ini ~/.config/spicetify/Themes/text/
-    cp -vr ~/.dotfiles/linux/yazi ~/.config/
-    cp -vr ~/.dotfiles/linux/i3 ~/.config/
+    get_diff_and_copy ~/.dotfiles/linux/clipton ~/.config/
+    get_diff_and_copy ~/.dotfiles/linux/.bashrc ~/
+    get_diff_and_copy ~/.dotfiles/linux/.bash_profile ~/
+    get_diff_and_copy ~/.dotfiles/linux/.zshrc ~/
+    get_diff_and_copy ~/.dotfiles/linux/polybar ~/.config/
+    get_diff_and_copy ~/.dotfiles/linux/.xinitrc ~/
+    get_diff_and_copy ~/.dotfiles/linux/.Xresources ~/
+    get_diff_and_copy ~/.dotfiles/linux/bat ~/.config/
+    get_diff_and_copy ~/.dotfiles/linux/kitty ~/.config/
+    get_diff_and_copy ~/.dotfiles/linux/rofi ~/.config/
+    get_diff_and_copy ~/.dotfiles/linux/spicetify/Themes/text/color.ini ~/.config/spicetify/Themes/text/
+    get_diff_and_copy ~/.dotfiles/linux/yazi ~/.config/
+    get_diff_and_copy ~/.dotfiles/linux/i3 ~/.config/
 fi
 
 
