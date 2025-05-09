cde91508980d6f79bf73a8f29bd888779f6e58d9
artzhk <mr.zhukovsky02@gmail.com>
2025-05-09 08:44:08 +0200

    chore(emacs): visual line mode

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -20,6 +20,8 @@
 
 (evil-mode)
 
+(setq visual-line-mode 1)
+
 (global-set-key (kbd "C-c l") #'org-store-link)
 (global-set-key (kbd "C-c a") #'org-agenda)
 (global-set-key (kbd "C-c c") #'org-capture)
